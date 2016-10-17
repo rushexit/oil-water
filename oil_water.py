32f7cdd8de456213fcc7f05ae9dcbdf06d51be77
Rachel Garza <rachelgarzadaring@gmail.com>
2016-10-17 10:41:26 -0500

    moved ow_main file to components folder.

--- oil_water.py
+++ oil_water.py
@@ -1,8 +1,8 @@
-import sys, pygame, data.components.oil_water_main, OpenGL
+import sys, pygame, data.components.ow_main, OpenGL
 # START SCREEN
 
 # initialize save file.
 
 # ACT I
 ## SCENE I
-data.components.oil_water_main.scriptHandler("test_script.txt")
+data.components.ow_main.scriptHandler("test_script.txt")
