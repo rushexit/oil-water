4f7871ccbb0d7efc3dd608d727fd7ffcdecf91bb
Rachel Garza <rachelgarzadaring@gmail.com>
2016-10-24 13:44:16 -0500

    added beginning of start_screen functionality.

---
+++ data/plotline_data/start_screen.py
@@ -0,0 +1,7 @@
+import sys, pygame, OpenGL
+
+pygame.init()
+mainMenu_width = 300
+mainMenu_height = 500
+font = pygame.font.Font("freesansbold.ttf", 10)
+mainMenu = pygame.Surface((mainMenu_width, mainMenu_height))
