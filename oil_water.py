870a932866f02a44ea06a28fa53f25aa70387fbf
Rachel Garza <rachelgarzadaring@gmail.com>
2016-10-25 17:26:20 -0500

    nothing to see here... seriously.

--- oil_water.py
+++ oil_water.py
@@ -6,5 +6,5 @@ import sys, pygame, data.components.ow_main, OpenGL
 
 # ACT I
 ## SCENE I
-# data.components.ow_main.scriptHandler("test_script.txt")
-data.components.ow_main.heartTimer(pygame.KEYDOWN, data.components.ow_main.scriptHandler("test_script.txt"))
+data.components.ow_main.scriptHandler("test_script.txt")
+# data.components.ow_main.heartTimer(pygame.KEYDOWN, data.components.ow_main.scriptHandler("test_script.txt"))
