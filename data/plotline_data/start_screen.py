51da6bec83f85f48bbd43f5ffb7767e0cac48f2b
Rachel Garza <rachelgarzadaring@gmail.com>
2016-10-25 10:59:45 -0500

    updated start_screen file, testing transparent surfaces.

--- data/plotline_data/start_screen.py
+++ data/plotline_data/start_screen.py
@@ -1,7 +1,28 @@
 import sys, pygame, OpenGL
 
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
 pygame.init()
-mainMenu_width = 300
-mainMenu_height = 500
-font = pygame.font.Font("freesansbold.ttf", 10)
-mainMenu = pygame.Surface((mainMenu_width, mainMenu_height))
+display_width = 800
+display_height = 500
+screen = pygame.display.set_mode((display_width, display_height))
+bgImage = pygame.image.load("spacetrees.png")
+
+trans = pygame.Surface((300, 500))
+trans.fill(black)
+pygame.Surface.convert_alpha(trans)
+trans.set_alpha(50)
+screen.blit(bgImage, (0,0))
+screen.blit(trans, ((display_width - 300) / 2, 0))
+
+def gameLoop():
+	gameRunning = True
+	while gameRunning == True:
+		if pygame.event.get(pygame.QUIT):
+			pygame.quit()
+			sys.exit()
+		pygame.display.update()
+
+gameLoop()
