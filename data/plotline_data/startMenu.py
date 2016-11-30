2692aadcb01bf28cb99e6dc09515bd6c76eecb7e
rachel garza-daring <rachelgarzadaring@gmail.com>
2016-11-29 20:40:10 -0600

    getting even closer with the startMenu.

--- data/plotline_data/startMenu.py
+++ data/plotline_data/startMenu.py
@@ -10,7 +10,7 @@ display_height = 500
 
 font = pygame.font.Font("freesansbold.ttf", 10)
 screen = pygame.display.set_mode((display_width, display_height))
-screen.fill(white)
+screen.fill(black)
 pygame.display.set_caption('OIL//WATER')
 pygame.display.update()
 def new_game():
@@ -32,50 +32,65 @@ class menuItem():
 		self.buttonSurface = self.buttonSurface.convert_alpha()
 		self.buttonSurface.fill(white)
 		print "text: " + str(text)
-		for index, item in enumerate(functions):
-			print "enumerate: " + str(index) + " : " + item
+#		for index, item in enumerate(functions):
+#			print "enumerate: " + str(index) + " : " + item
 	def setPosition(self, x, y):
-		menuButtonXPos = 0
 		yPos = (display_height / 1.35)
 		self.position = x, y
 	def setOpacity(self, opacity):
 		self.opacity = opacity
 		self.buttonSurface.set_alpha(self.opacity)
-def startMenu():
-	menuRunning = True
-	optionPosition = 0
-	# add menuItem and positioning functions here.
-	while menuRunning == True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-			if event.type == pygame.KEYDOWN:
-				key = pygame.key.get_pressed()
-				if key[pygame.K_a]:
-					if 0 < optionPosition <= 2:
-						optionPosition -= 1
-						print "optionPosition: " + str(optionPosition)
-						print "K_a pressed."
-				if key[pygame.K_d]:
-					if 0 <= optionPosition < 2:
-						optionPosition += 1
-						print "K_d pressed."
-						print "optionPosition: " + str(optionPosition)
-				# add change opacity function here
-				if key[pygame.K_SPACE]:
-					print "option selected."
-					if optionPosition == 0:
-						startFunctions["new game"]()
-					elif optionPosition == 1:
-						startFunctions["load game"]()
-					elif optionPosition == 2:
-						startFunctions["exit game"]()
-					menuRunning = False
+
+class startMenu():
+	def __init__(self, functions):
+		self.menuRunning = True
+		self.optionPosition = 0
+		defaultOpacity = 0
+		menuButtonXPos = 0
+		for item in functions:
+			print "do this do that."
+			print item
+			menuButton = menuItem(item, functions[item], defaultOpacity)
+			menuButton.setPosition(menuButtonXPos, 0)
+			screen.blit(menuButton.buttonSurface, menuButton.position)
+			menuButtonXPos += menuButton.buttonSurface.get_width()
 			
+	def run(self):
+		while self.menuRunning == True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					key = pygame.key.get_pressed()
+					if key[pygame.K_a]:
+						if 0 < self.optionPosition <= 2:
+							self.optionPosition -= 1
+							print "optionPosition: " + str(self.optionPosition)
+							print "K_a pressed."
+					if key[pygame.K_d]:
+						if 0 <= self.optionPosition < 2:
+							self.optionPosition += 1
+							print "K_d pressed."
+							print "optionPosition: " + str(self.optionPosition)
+					# add change opacity function here
+					if self.optionPosition == 0:
+						self.menuButton.setOpacity(100)
+						if key[pygame.K_SPACE]:
+							startFunctions["new game"]()
+							self.menuRunning = False
+					elif self.optionPosition == 1:
+						self.menuButton.setOpacity(100)
+						if key[pygame.K_SPACE]:
+							startFunctions["load game"]()
+							self.menuRunning = False
+					elif self.optionPosition == 2:
+						self.menuButton.setOpacity(100)
+						if key[pygame.K_SPACE]:
+							startFunctions["exit game"]()
+							self.menuRunning = False
+
 startFunctions = {"new game" : new_game, "load game" : load_game, "exit game" : exit_game}
 
-test_mi = menuItem(startFunctions.keys(), startFunctions, 100)
-test_mi.setOpacity(100)
-print test_mi.opacity
 print startFunctions["new game"]
-startMenu()
+sm = startMenu(startFunctions)
+sm.run()
